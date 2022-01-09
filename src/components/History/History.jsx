8e66a267fd9edec3061287ec10f492dfa8f82f3b
Yasir Uddin <yasir.uddin@icloud.com>
2022-01-09 00:48:37 -0700

    updated history table

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -2,182 +2,87 @@ import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {useHistory} from 'react-router-dom';
 import { useParams} from 'react-router-dom';
-// import './History.css';
-import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 
 
+function History() {
+
+  // define dispatch in order to use it
+  const dispatch = useDispatch();
+
+  // define history in order to route to page
+  const history = useHistory();
+
+  // define params
+  const params = useParams();
+
+  // grab reducer from the redux store via useSelector
+  const results = useSelector(store => store.resultReducer);
 
 
-// function History() {
+  const handleDeletebtn = (id) => {
 
-//   // define dispatch in order to use it
-//   const dispatch = useDispatch();
+    dispatch({
+        type: 'DELETE_RESULT',
+        payload: id
+    })
+};
 
-//   // define history in order to route to page
-//   const history = useHistory();
+const handleEditbtn = (id) => {
+  console.log('test');
+  console.log('id', id);
+  history.push(`/edit_results/${id}`);
+};
 
-//   // define params
-//   const params = useParams();
-
-//   // grab reducer from the redux store via useSelector
-//   const results = useSelector(store => store.resultReducer);
-//   // const diet = useSelector(store => store.diet);
-
-//   const handleDeletebtn = (id) => {
-
-//     dispatch({
-//         type: 'DELETE_RESULT',
-//         payload: id
-//     })
-// };
-
-// const handleEditbtn = (id) => {
-//   console.log('test');
-//   console.log('id', id);
-//   history.push(`/edit_results/${id}`);
-// };
-// // const handleEditbtn = (id) => {
-// //   history.push(`/edit_results/`);
-// // };
-
-//   return (
-//     <div className="container">
-//       <p>History:</p>
-//       <p class="response">Here is the table header </p>
-//       <p>
-//         {results.map(result => {
-//             return (
-//                 <p class="response">{result.physical_activity} {result.diet} {result.sleep} {result.mood} {result.comments} <button onClick={() => handleEditbtn(result.id)}>EDIT</button> <button onClick={() =>handleDeletebtn(result.id)}>DELETE</button>
-//                 </p>
-//             )
-//         })}
-//       </p>
-
-//     </div>
-
-
-//   );
-// }
-
-// export default History;
-
-const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
-  },
-];
-
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
-
-export default function StickyHeadTable() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
 
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
+    <div className="container">
+
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Physical Activity</TableCell>
+            <TableCell align="center">Diet</TableCell>
+            <TableCell align="center">Sleep</TableCell>
+            <TableCell align="center">Mood</TableCell>
+            <TableCell align="center">Comments</TableCell>
+            <TableCell align="center">Edit</TableCell>
+            <TableCell align="center">Delete</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {results.map((result) => (
+            <TableRow
+              key={result.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="center">
+                {result.physical_activity}
+              </TableCell>
+              <TableCell align="center">{result.diet}</TableCell>
+              <TableCell align="center">{result.sleep}</TableCell>
+              <TableCell align="center">{result.mood}</TableCell>
+              <TableCell align="center">{result.comments}</TableCell>
+              <TableCell align="center"><Button color="success" variant="outlined" onClick={() => handleEditbtn(result.id)}>EDIT</Button></TableCell>
+              <TableCell align="center"><Button color="error" variant="outlined" onClick={() =>handleDeletebtn(result.id)}>DELETE</Button></TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+    </div>
+
+
   );
 }
+
+export default History;
