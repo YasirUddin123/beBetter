9ee7c08a815d6752b7d23ad1add2e7d4af18c573
Yasir Uddin <yasir.uddin@icloud.com>
2022-01-09 14:39:32 -0700

    test to make sure functionality still works

--- src/components/EditResultsPage/EditResultsPage.jsx
+++ src/components/EditResultsPage/EditResultsPage.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 import { useEffect } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './EditResults.css'
 
 
 
@@ -18,19 +15,6 @@ function EditResultsPage() {
     // Grab our reducer for editing
     const editResult = useSelector(store => store.editThisResult)
     // Now, let's go check out the reducer on the editResult.reducer.js file
-
-    function createData(name, calories, fat, carbs, protein) {
-        return { name, calories, fat, carbs, protein };
-      }
-
-      const rows = [
-        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-        createData('Eclair', 262, 16.0, 24, 6.0),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
-      ];
-
 
     useEffect(() => {
         console.log('🚀🚀🚀🚀🚀🚀🚀');
@@ -77,6 +61,7 @@ function EditResultsPage() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log('test')
         dispatch({
         type: 'EDIT_RESULT',
         payload: {
@@ -91,83 +76,78 @@ function EditResultsPage() {
     }
 
 return (
-<div>
-    <h2>Edit Results:</h2>
-
+<div class="editresults">
+    <h2>Edit Results</h2>
+<br />
+<br />
         <form onSubmit={handleSubmit}>
-            <input
+            <Box
+                  component="form"
+                  sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                  }}
+                  noValidate
+                  autoComplete="off"
+                />
+
+            <TextField
+            id="outlined-basic"
+            label="Physical Exercise"
+            variant="outlined"
             placeholder='Phys.Exer. Score'
             type="number"
             value={editResult.physical_activity || ''}
             onChange={handlePhysicalExerciseResultsChange}
             />
 
-            <input
+            <TextField
+            id="outlined-basic"
+            label="Diet"
+            variant="outlined"
             placeholder='Diet Score'
             type="number"
             value={editResult.diet || ''}
             onChange={handleDietResultsChange}
             />
 
-            <input
+            <TextField
+            id="outlined-basic"
+            label="Sleep"
+            variant="outlined"
             placeholder='Sleep Score'
             type="number"
             value={editResult.sleep || ''}
             onChange={handleSleepResultsChange}
             />
 
-            <input
+            <TextField
+            id="outlined-basic"
+            label="Mood"
+            variant="outlined"
             placeholder='Mood Score'
             type="number"
             value={editResult.mood || ''}
             onChange={handleMoodResultsChange}
             />
 
-            <input
+            <TextField
+            id="outlined-basic"
+            label="Comments"
+            variant="outlined"
             placeholder='Comments'
             type="text"
             value={editResult.comments || ''}
             onChange={handleCommentsResultsChange}
             />
-
-            <button>Update</button>
+<br />
+<br />
+<br />
+<Button color="success" variant="outlined" size="large" align="center" onClick={handleSubmit}>Update</Button>
         </form>
 
-        <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-          <TableCell align="center">Physical Activity</TableCell>
-            <TableCell align="center">Diet</TableCell>
-            <TableCell align="center">Sleep</TableCell>
-            <TableCell align="center">Mood</TableCell>
-            <TableCell align="center">Comments</TableCell>
-            <TableCell align="center">Edit</TableCell>
-            <TableCell align="center">Delete</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
 </div>
 );
 }
-
 
 export default EditResultsPage;
 
