5f1b3b92a39e11ec2cbb37beb5940451d7772578
Yasir Uddin <yasir.uddin@icloud.com>
2022-01-10 15:01:30 -0700

    npm install chart.js and loaded up a sample graph

--- src/components/GraphPage/GraphPage.jsx
+++ src/components/GraphPage/GraphPage.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
+import {Line} from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
+import {CategoryScale} from 'chart.js'; Chart.register(CategoryScale)
 // This is one of our simplest components
 // It doesn't have local state
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is
 
 function GraphPage() {
+
+  const state = {
+    labels: ['January', 'February', 'March','April', 'May'],
+    datasets: [
+      {
+        label: 'Sample Data',
+        fill: false,
+        lineTension: 0.5,
+        backgroundColor: 'rgba(75,192,192,1)',
+        borderColor: 'rgba(0,0,0,1)',
+        borderWidth: 2,
+        data: [65, 59, 80, 81, 56]
+      }
+    ]
+  }
+
   return (
     <div className="container">
-      <p>Graph Will Be Here</p>
+      <p>Graph!</p>
+ <Line
+          data={state}
+          options={{
+            title:{
+              display:true,
+              text:'Average Rainfall per month',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right'
+            }
+          }}
+        />
       </div>
   );
 }
