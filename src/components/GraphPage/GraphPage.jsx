80afbfe3e5ea2a062fc7ff2045913e57a49e71ff
Yasir Uddin <yasir.uddin@icloud.com>
2022-01-10 23:48:24 -0700

    able to load one data category

--- src/components/GraphPage/GraphPage.jsx
+++ src/components/GraphPage/GraphPage.jsx
@@ -1,33 +1,97 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Line} from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
 import {CategoryScale} from 'chart.js'; Chart.register(CategoryScale)
-// This is one of our simplest components
-// It doesn't have local state
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is
+import { useSelector, useDispatch } from 'react-redux';
 
 function GraphPage() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch({ type: 'FETCH_RESULT'})
+}, []);
 
+const results = useSelector(store => store.resultReducer);
+
+let newDiet = [];
+let newLabel = [];
+
+function getDiet(){
+  for(let i = 0; i < results.length; i++){
+    newDiet.push(results[i].diet)
+  }
+  return newDiet;
+}
+
+// function getLabel(){
+//   for(let i = 0; i < results.length; i++){
+//     state.labels.push('label')
+//   }
+//   return newLabel;
+// }
+
+// function addDietData(chart, label, data) {
+//   chart.data.labels.push(label);
+//   chart.data.datasets.forEach((dataset) => {
+//       dataset.data.push(data);
+//   });
+//   chart.update();
+// }
+
+function addData(chart, label, data) {
+  chart.state.labels.push(label);
+  chart.state.datasets.forEach((dataset) => {
+      dataset.state.push(data);
+  });
+  chart.update();
+}
+
+
+
+
+
+getDiet();
+console.log(newDiet);
+console.log(results);
   const state = {
-    labels: ['January', 'February', 'March','April', 'May'],
+    labels: ['Log1', 'Log2', 'Log3', 'Log4', 'Log5'],
     datasets: [
       {
-        label: 'Sample Data',
+        label: 'Your Diet Scores',
         fill: false,
         lineTension: 0.5,
         backgroundColor: 'rgba(75,192,192,1)',
         borderColor: 'rgba(0,0,0,1)',
         borderWidth: 2,
-        data: [65, 59, 80, 81, 56]
+        data:newDiet
       }
-    ]
+      // {
+      //   label: 'My Scores',
+      //   fill: false,
+      //   lineTension: 0.5,
+      //   backgroundColor: 'rgba(75,192,192,1)',
+      //   borderColor: 'rgba(0,0,0,1)',
+      //   borderWidth: 2,
+      //   data: [50, 65, 75, 65, 70]
+      // }
+
+    ],
+  }
+
+  function addData(chart, labels, data) {
+    chart.state.labels.push('new log');
+    chart.state.datasets.forEach((dataset) => {
+        dataset.data.push(newDiet[newDiet.length-1]);
+    });
+    state.update();
   }
 
   return (
     <div className="container">
       <p>Graph!</p>
- <Line
+<Line
+          // data={results.map((result) => (
+          //   {result, diet}
+          // ))}
           data={state}
           options={{
             title:{
