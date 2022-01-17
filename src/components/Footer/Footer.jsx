91311b1fdaf6712e9ef778579f97874cc277839d
Yasir Uddin <yasir.uddin@icloud.com>
2022-01-16 23:58:55 -0700

    footer comments

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Footer.css';
 
+
+// This is maintained in order to make sure the styling of the background page is contained
 function Footer() {
   return <footer></footer>;
 }
